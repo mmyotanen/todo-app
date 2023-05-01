751187bc091d987c7fe303c6c1d5f3b960ca89e8
Miika Myötänen <miikamyotanen@gmail.com>
2023-05-01 20:16:53 +0300

    added fonts and todo appending to file

--- gui.py
+++ gui.py
@@ -2,10 +2,22 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo")
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
 add_button = sg.Button("Add")
 # exit_button =
 
-window = sg.Window("My to-do App", layout=[[label], [input_box, add_button]])
-window.read()
+window = sg.Window("My to-do App",
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
+while True:
+    event, values = window.read()
+    if event == "Add":
+        todos = functions.get_todos()
+        new_todo = values['todo'] + "\n"
+        todos.append(new_todo)
+        functions.add_todos(todos)
+    if event == sg.WIN_CLOSED:
+        break
+
+
 window.close()
